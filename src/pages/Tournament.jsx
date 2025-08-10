348d55a3cc36cee835e1e970c4d48f5ba4c37e50
Omar Abdo <omar30abdo30@gmail.com>
2025-08-11 02:43:50 +0300

    update small screen view

--- src/pages/Tournament.jsx
+++ src/pages/Tournament.jsx
@@ -120,7 +120,7 @@ function Tournament() {
             {winnersList[winnersList.length - 1].every((w) => w !== null) &&
                 winnersList[winnersList.length - 1].length > 1 && (
                     <button
-                        className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 transition"
+                        className="nextBtn bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 transition"
                         onClick={handleNext}
                     >
                         Next Round
